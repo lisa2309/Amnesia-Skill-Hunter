849dcdd0ba9ffce39bcf3ebdb810378cc8af0eac
lsfr62 <lucas.scahefer@mni.thm.de>
2022-02-02 13:12:21 +0100

    Gerüst für die Fähigkeitenübernahme implementiert

--- gddpl/Assets/PlayerCharacter/Scripts/PlayerShoot.cs
+++ gddpl/Assets/PlayerCharacter/Scripts/PlayerShoot.cs
@@ -13,6 +13,7 @@ public class PlayerShoot : MonoBehaviour
     //state
     private bool shooting;
     private Coroutine currentSpawnBulletInstance;
+    private Ability currentAbility = Ability.None;
 
     //config
     [Header("Shooting")]
@@ -34,6 +35,7 @@ public class PlayerShoot : MonoBehaviour
         controls.Gameplay.Power.performed += context => StartShooting();
         controls.Gameplay.Power.canceled += context => StopShooting();
     }
+
     private void Start()
     {
         animator = GetComponent<Animator>();
@@ -42,6 +44,27 @@ public class PlayerShoot : MonoBehaviour
     }
 
     private void StartShooting()
+    {
+        switch (currentAbility)
+        {
+            case Ability.Fireball:
+                ShootFireball();
+                break;
+            case Ability.Bow:
+                ShootBow();
+                break;
+            case Ability.Dash:
+                Dash();
+                break;
+            case Ability.Stamp:
+                Stamp();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void ShootFireball()
     {
         shooting = true;
         currentSpawnBulletInstance = StartCoroutine(SpawnBullet());
@@ -56,6 +79,22 @@ public class PlayerShoot : MonoBehaviour
         //animation
         animator.SetBool("Shooting", true);
     }
+
+    private void ShootBow()
+    {
+
+    }
+
+    private void Dash()
+    {
+
+    }
+
+    private void Stamp()
+    {
+
+    }
+
     private void StopShooting()
     {
         shooting = false;
@@ -63,14 +102,55 @@ public class PlayerShoot : MonoBehaviour
 
         movement.ResetRunSpeedModifier();
 
-        //animation
+        switch (currentAbility)
+        {
+            case Ability.Fireball:
+                StopShootFireball();
+                break;
+            case Ability.Bow:
+                StopShootBow();
+                break;
+            case Ability.Dash:
+                StopDash();
+                break;
+            case Ability.Stamp:
+                StopStamp();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void StopShootFireball()
+    {
         animator.SetBool("Croushing", false);
     }
+
+    private void StopShootBow()
+    {
+
+    }
+
+    private void StopDash()
+    {
+
+    }
+
+    private void StopStamp()
+    {
+
+    }
+
     private IEnumerator SpawnBullet()
     {
         Instantiate(bulletPrefab, shootPoint.position, shootPoint.rotation);
         yield return new WaitForSeconds(bulletSpawnInterval);
         if (shooting) StartCoroutine(SpawnBullet());
+    }
+
+    private void GetAbilty(Ability ability)
+    {
+        this.currentAbility = ability;
     }
 
     private void OnEnable()
@@ -81,4 +161,13 @@ public class PlayerShoot : MonoBehaviour
     {
         controls.Disable();
     }
+}
+
+public enum Ability
+{
+    Fireball,
+    Bow,
+    Dash,
+    Stamp,
+    None
 }
