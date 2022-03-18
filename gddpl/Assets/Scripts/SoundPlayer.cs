8b1927389ffc95f6b7df14041ff756f8a4c6ddc7
Anna Lisa Müller <anna.mueler-2@mni.thm.de>
2022-03-18 12:33:04 +0100

    Music Volume StateController

--- gddpl/Assets/Scripts/SoundPlayer.cs
+++ gddpl/Assets/Scripts/SoundPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundPlayer : MonoBehaviour
 {
@@ -11,11 +12,21 @@ public class SoundPlayer : MonoBehaviour
 
     [SerializeField]
     private AudioSource fireSound;
+    [SerializeField]
+    private Slider slider;
+
+    void Start() {
+        attackSound.volume = StateController.currentSoundEffectVolume;
+        arrowSound.volume = StateController.currentSoundEffectVolume;
+        fireSound.volume = StateController.currentSoundEffectVolume;
+        slider.value = StateController.currentSoundEffectVolume;
+    }
 
     public void UpdateVolume(float volume)
 	{
 		attackSound.volume = volume;
         arrowSound.volume = volume;
         fireSound.volume = volume;
+        StateController.currentSoundEffectVolume = volume;
 	}
 }
