aa679a38917d5585fa5bb980471de1803377ebfa
Lars Sauer <lars.sauer@mni.thm.de>
2022-03-18 16:41:57 +0100

    skelHitbox/Dmg/Ai adjusted

--- gddpl/Assets/Scripts/SkeletonController.cs
+++ gddpl/Assets/Scripts/SkeletonController.cs
@@ -60,10 +60,12 @@ public class SkeletonController : MonoBehaviour
     private void FixedUpdate()
     {        
         if (WallOrGapAhead()) ChangeDirection();
-        if (PlayerVisible())
+        if (PlayerVisible() | GetDistance() <= range)
         {
+            Debug.Log ("SEE PLAYER");
             if (GetDistance() <= range)
             {
+                Debug.Log("IN RANGE PLAYER");
                 if (Time.time > lastAttackedAt + attackCooldown)
                 {
                     Attack();
@@ -170,6 +172,8 @@ public class SkeletonController : MonoBehaviour
 
     private float GetDistance()
     {
-        return Vector2.Distance(this.transform.position, player.position);
+        if (PlayerVisible())
+            return Vector2.Distance(this.transform.position, player.position);
+        else return 100;
     }
 }
